a171f560041682cdbbf7e7a09c42fed8a0520f86
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-18 12:11:10 +0100

    Feat: Implement Genre enum

--- app/src/main/java/com/example/recordshopfrontend/model/Genre.java
+++ app/src/main/java/com/example/recordshopfrontend/model/Genre.java
@@ -1,2 +1,17 @@
-package com.example.recordshopfrontend.model;public enum Genre {
+package com.example.recordshopfrontend.model;
+
+public enum Genre {
+    TRANCE("Trance"),
+    HOUSE("House"),
+    ROCK("Rock"),
+    HIPHOP("Hip Hop"),
+    POP("Pop"),
+    METAL("Metal"),
+    DNB("Drum & Bass");
+
+    public final String message;
+
+    Genre(String message) {
+        this.message = message;
+    }
 }
