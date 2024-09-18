4dfd4b80e9d2b57446d54c6affe6e620c11405c4
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-18 12:33:21 +0100

    Feat: Implement Retrofit Instance

--- app/src/main/java/com/example/recordshopfrontend/service/RetrofitInstance.java
+++ app/src/main/java/com/example/recordshopfrontend/service/RetrofitInstance.java
@@ -1,2 +1,28 @@
-package com.example.recordshopfrontend.service;public class RetrofitInstance {
+package com.example.recordshopfrontend.service;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
+
+    public static AlbumsApiService getService() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+
+        return retrofit.create(AlbumsApiService.class);
+    }
 }
