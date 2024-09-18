eb5dfb8d0dd8a176370dfe1cf366c3750af82d51
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-18 21:59:56 +0100

    Feat: Implement Album Adapter

---
+++ app/src/main/java/com/example/recordshopfrontend/ui/mainactivity/AlbumAdapter.java
@@ -0,0 +1,63 @@
+package com.example.recordshopfrontend.ui.mainactivity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recordshopfrontend.R;
+import com.example.recordshopfrontend.databinding.AlbumLayoutBinding;
+import com.example.recordshopfrontend.model.Albums;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder> {
+
+    List<Albums> albumsList;
+    Context context;
+
+    public AlbumAdapter(List<Albums> albums, Context context) {
+        this.albumsList = albums;
+        this.context = context;
+    }
+
+    public static class AlbumViewHolder extends RecyclerView.ViewHolder {
+
+        private AlbumLayoutBinding albumItemBinding;
+
+        public AlbumViewHolder(AlbumLayoutBinding albumItemBinding) {
+            super(albumItemBinding.getRoot());
+            this.albumItemBinding = albumItemBinding;
+        }
+    }
+
+    @NonNull
+    @Override
+    public AlbumViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        AlbumLayoutBinding binding = DataBindingUtil
+                .inflate(
+                        LayoutInflater.from(parent.getContext()),
+                        R.layout.album_layout,
+                        parent,
+                        false);
+
+        return new AlbumViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
+        Albums albums = albumsList.get(position);
+        holder.albumItemBinding.setAlbums(albums);
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return albumsList.size();
+    }
+}
