e0025a8778ce9d700cb4cdd4acba6581b2609299
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-19 16:50:46 +0100

    Feat: Implement button handlers for post album & onBack

---
+++ app/src/main/java/com/example/recordshopfrontend/ui/addalbum/AddAlbumClickHandlers.java
@@ -0,0 +1,52 @@
+package com.example.recordshopfrontend.ui.addalbum;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.recordshopfrontend.model.Albums;
+import com.example.recordshopfrontend.ui.MainActivity;
+import com.example.recordshopfrontend.ui.mainactivity.MainActivityViewModel;
+
+public class AddAlbumClickHandlers {
+
+    private Albums albums;
+    private Context context;
+    private MainActivityViewModel mainActivityViewModel;
+
+    public AddAlbumClickHandlers(Albums albums, Context context, MainActivityViewModel mainActivityViewModel) {
+        this.albums = albums;
+        this.context = context;
+        this.mainActivityViewModel = mainActivityViewModel;
+    }
+
+    public void onSubmitClickHandler(View view) {
+
+        if (albums.getArtist() == null || albums.getAlbumName() == null || albums.getGenre() == null || albums.getReleaseYear() <= 1900 || albums.getStock() <= 0) {
+            Toast.makeText(context, "Field cannot be empty", Toast.LENGTH_SHORT).show();
+        } else {
+
+            Intent intent = new Intent(context, MainActivity.class);
+
+            Albums newAlbums = new Albums(
+                    albums.getId(),
+                    albums.getStock(),
+                    albums.getArtist(),
+                    albums.getReleaseYear(),
+                    albums.getAlbumName(),
+                    albums.getGenre()
+            );
+
+            mainActivityViewModel.postAlbum(newAlbums);
+
+            context.startActivity(intent);
+        }
+    }
+
+    public void onBackButtonClicked(View view) {
+        Intent intent = new Intent(context, MainActivity.class);
+
+        context.startActivity(intent);
+    }
+}
