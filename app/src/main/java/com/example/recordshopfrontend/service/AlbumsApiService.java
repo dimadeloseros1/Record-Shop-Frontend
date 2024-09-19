c3e000fb6b06a5200d41e01c9a6d015ffe069714
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-19 12:28:39 +0100

    Feat: Implement POST method to AlbumApiService

--- app/src/main/java/com/example/recordshopfrontend/service/AlbumsApiService.java
+++ app/src/main/java/com/example/recordshopfrontend/service/AlbumsApiService.java
@@ -5,10 +5,15 @@ import com.example.recordshopfrontend.model.Albums;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumsApiService {
 
     @GET("albums")
     Call<List<Albums>> getAllAlbums();
+
+    @POST("albums")
+    Call<Albums> postAlbums(@Body Albums albums);
 }
