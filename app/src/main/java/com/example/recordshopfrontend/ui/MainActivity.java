57117a2ed136b25881874533a58c29b5a7e16dc4
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-23 15:10:44 +0100

    Feat: Implement searchView and it's default methods

--- app/src/main/java/com/example/recordshopfrontend/ui/MainActivity.java
+++ app/src/main/java/com/example/recordshopfrontend/ui/MainActivity.java
@@ -4,6 +4,8 @@ import static android.provider.MediaStore.Audio.AudioColumns.ALBUM_KEY;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,12 +39,31 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
     private ActivityMainBinding binding;
     private MainActivityClickHandler clickHandler;
     private static final String ALBUMS_KEY = "albums";
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        searchView = findViewById(R.id.searchView);
+        searchView.clearFocus();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                filterList(e);
+
+                return true;
+            }
+        });
 
         binding = DataBindingUtil.setContentView(
                 this,
@@ -87,6 +108,26 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
 
         intent.putExtra(ALBUMS_KEY, albumsList.get(position));
         startActivity(intent);
+
+    }
+
+    private void filterList(String text) {
+
+        albumsList = new ArrayList<>();
+
+        for (Albums albums : albumsList) {
+
+            if (albums.getArtist().toLowerCase().contains(text.toLowerCase())) {
+                albumsList.add(albums);
+            }
+        }
+
+        if (albumsList.isEmpty()) {
+            Toast.makeText(this, "No album found", Toast.LENGTH_SHORT).show();
+        } else {
+            albumAdapter.setFilteredList(albumsList);
+        }
+
 
     }
 }
