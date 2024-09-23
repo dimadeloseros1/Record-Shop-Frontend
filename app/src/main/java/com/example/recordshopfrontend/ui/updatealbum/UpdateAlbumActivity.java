8bf274f5004ae3536249b35e42fb4dc10f394ac5
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-23 10:49:32 +0100

    Feat: Implement condition for albums object so the app does not crash

--- app/src/main/java/com/example/recordshopfrontend/ui/updatealbum/UpdateAlbumActivity.java
+++ app/src/main/java/com/example/recordshopfrontend/ui/updatealbum/UpdateAlbumActivity.java
@@ -1,26 +1,59 @@
 package com.example.recordshopfrontend.ui.updatealbum;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.recordshopfrontend.R;
+import com.example.recordshopfrontend.databinding.ActivityAddNewAlbumBinding;
+import com.example.recordshopfrontend.databinding.ActivityUpdateAlbumBinding;
+import com.example.recordshopfrontend.model.Albums;
+import com.example.recordshopfrontend.ui.mainactivity.MainActivityViewModel;
 
 public class UpdateAlbumActivity extends AppCompatActivity {
 
+    private ActivityUpdateAlbumBinding binding;
+    private UpdateAlbumClickHandlers handlers;
+    private Albums albums;
+    private static final String ALBUMS_KEY = "albums";
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_update_album);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        albums = getIntent().getParcelableExtra(ALBUMS_KEY, Albums.class);
+        if (albums == null) {
+
+            finish();
+            return;
+        }
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_update_album
+        );
+
+        MainActivityViewModel viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+
+        handlers = new UpdateAlbumClickHandlers(
+                albums,
+                this,
+                viewModel
+        );
+
+        binding.setAlbum(albums);
+        binding.setClickHandlers(handlers);
     }
 }
